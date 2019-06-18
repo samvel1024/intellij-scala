f13275b7ec64f9303b81caa9c2e6bfd0c4fb55a6
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2019-06-18 17:49:07 +0200

    Use scala varsion 2.13 in ScalaClausesCompletionTest
     #SCL-15634

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/completion3/ScalaClausesCompletionTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/completion3/ScalaClausesCompletionTest.scala
@@ -4,7 +4,6 @@ package completion3
 
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.codeInsight.lookup.{Lookup, LookupElement}
-import org.jetbrains.plugins.scala.debugger.{ScalaVersion, Scala_2_12}
 
 class ScalaClausesCompletionTest extends ScalaCodeInsightTestBase {
 
@@ -12,8 +11,6 @@ class ScalaClausesCompletionTest extends ScalaCodeInsightTestBase {
   import Lookup.REPLACE_SELECT_CHAR
   import ScalaCodeInsightTestBase._
   import completion.ScalaKeyword.{CASE, MATCH}
-
-  override implicit val version: ScalaVersion = Scala_2_12
 
   def testSyntheticUnapply(): Unit = doPatternCompletionTest(
     fileText =
